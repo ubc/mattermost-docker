a92af254e0d505d5bc79628074e80f3133307930
Pan Luo <pan.luo@ubc.ca>
2017-02-12 21:36:14 -0800

    WIP update for alpine image
    Not working right now. May need build mattermost with musl lib

--- web/docker-entry.sh
+++ web/docker-entry.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 echo Starting Nginx
-sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost
-sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost-ssl
+sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/conf.d/mattermost
+sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/conf.d/mattermost-ssl
 if [ "$MATTERMOST_ENABLE_SSL" = true ]; then
     ssl="-ssl"
 fi
-ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/sites-enabled/mattermost
+ln -sf /etc/nginx/conf.d/mattermost$ssl /etc/nginx/conf.d/mattermost.conf
 nginx -g 'daemon off;'
